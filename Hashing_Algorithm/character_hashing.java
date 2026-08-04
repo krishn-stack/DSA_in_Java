8d663d3387d2bdd42a689035a1e4eab57ed78ec6
4nh51rk <krishnarastogi21022006@gmail.com>
2026-08-04 18:41:01 +0000

    Hashing_Algorithm

---
+++ Hashing_Algorithm/character_hashing.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class character_hashing{
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine().toLowerCase();
+
+        int hash[] = new int[256];
+        for(int i=0;i<str.length();i++){
+            hash[str.charAt(i)]++;
+        }
+
+        // for(int i=0;i<26;i++){
+        //     System.out.print(hash[i]+" ");
+        // }
+            
+        System.out.println("Enter how many queries you want to check for: ");
+        int num = sc.nextInt();sc.nextLine();
+        for(int i=0;i<num;i++){
+            System.out.print("Enter the query: ");
+            char ch = sc.nextLine().charAt(0);
+            System.out.println("The frequency of " + ch +" is: " + hash[ch]);
+        }
+    sc.close();
+    }
+}
---
+++ Hashing_Algorithm/number_hashing.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class number_hashing {
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of the array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements: ");
+        for(int i=0;i<n;i++)
+            arr[i] = sc.nextInt();
+
+        //creating a hash array to store the frequency of numbers
+        int hash[] = new int[100];
+        for(int i=0;i<n;i++)
+            hash[arr[i]]++;
+
+        //accessing the frequency of numbers
+        System.out.println("Enter how many number you want to check for: ");
+        int num = sc.nextInt();
+        for(int i=0;i<num;i++){
+            System.out.print("Enter the numbers: ");
+            int a = sc.nextInt();
+            System.out.println("The frequency of " + a +" is: " + hash[a]);
+        }
+    sc.close();
+    }
+}

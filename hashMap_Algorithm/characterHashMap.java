b03d0015fc468285b426e8afee590d03a7690184
4nh51rk <krishnarastogi21022006@gmail.com>
2026-08-06 13:10:19 +0000

    character hashing

--- hashMap_Algorithm/characterHashMap.java
+++ hashMap_Algorithm/characterHashMap.java
@@ -6,6 +6,7 @@ public class characterHashMap {
         System.out.print("Enter a string: ");
         String str = sc.nextLine().toLowerCase();
 
+        //creating a hashmap for hashing characters of a string
         HashMap<Character, Integer> hashMap = new HashMap<>();
         for(int i=0;i<str.length();i++){
             char ch = str.charAt(i);
@@ -15,11 +16,13 @@ public class characterHashMap {
                 hashMap.put(ch, 1);
         }
 
+        //printing the hashmap
         System.out.println("The frequency of the characters in the string are: ");
         for(Map.Entry<Character, Integer> element : hashMap.entrySet()){
             System.out.println(element.getKey() + " -> " + element.getValue());
         }
 
+        //fetching the frequency of the characters using hashMap
         System.out.print("Enter the number of queries you want to check for: ");
         int queries = sc.nextInt();sc.nextLine();
         for(int i=0;i<queries;i++){

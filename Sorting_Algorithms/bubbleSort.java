b85d84e25367c01e9ae0b5096db1fe60374f6be5
4nh51rk <krishnarastogi21022006@gmail.com>
2026-08-08 19:56:47 +0000

    modify

--- Sorting_Algorithms/bubbleSort.java
+++ Sorting_Algorithms/bubbleSort.java
@@ -1,31 +1,24 @@
 package Java.Sorting_Algorithms;
 import java.util.*;
-public class selectionSort {
+public class bubbleSort{
     public static void main(){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter siz eof the array: ");
+        System.out.print("Enter size of the array: ");
         int n = sc.nextInt();
         int a[] = new int[n];
         System.out.println("Enter the elements in the array: ");
         for(int i=0;i<n;i++)
             a[i] = sc.nextInt();
 
-        int small = 0, minIndex = 0;
         for(int i=0;i<n-1;i++){
-            small = a[i];
-            minIndex = i;
-            for(int j= i+1;j<n;j++){
-                if(small > a[j]){
-                    small = a[j];
-                    minIndex = j;
+            for(int j=0;j<n-i-1;j++){
+                if(a[j] > a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
                 }
             }
-            if(i != minIndex){
-                a[minIndex] = a[i];
-                a[i] = small;
-            }
         }
-
         System.out.println("----Sorted Array----");
         for(int i=0;i<n;i++)
             System.out.print(a[i] + " ");
@@ -33,4 +26,3 @@ public class selectionSort {
         sc.close();
     }
 }
-

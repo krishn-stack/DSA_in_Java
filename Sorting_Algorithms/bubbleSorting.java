8e9e155f4b429fbf478ca7f0534c6ddc516075cb
4nh51rk <krishnarastogi21022006@gmail.com>
2026-08-08 20:29:51 +0000

    modify

--- Sorting_Algorithms/bubbleSorting.java
+++ Sorting_Algorithms/bubbleSorting.java
@@ -2,14 +2,21 @@ package Java.Sorting_Algorithms;
 import java.util.*;
 public class bubbleSorting{
     static void bubbleSort(int a[], int n){
+
+        int didSwap = 0;  //optimization, T.C -> O(n^2) -> O(n)
+
         for(int i=0;i<n-1;i++){
             for(int j=0;j<n-i-1;j++){
                 if(a[j] > a[j+1]){
                     int temp = a[j];
                     a[j] = a[j+1];
                     a[j+1] = temp;
+                    didSwap = 1;    // if there is a swap, didSwap will be 1, else it will be 0
                 }
             }
+            if(didSwap == 0)    // if there is no swap, it means the array is already sorted, so we can break the loop
+                break;          // Time Complexity changes from O(n^2) to O(n) in best case when the array is already sorted,    
+                               // if we optimize the code by adding a didSwap flag to check if there is a swap or not.
         }
     }
 
@@ -23,7 +30,7 @@ public class bubbleSorting{
             a[i] = sc.nextInt();
 
         bubbleSort(a, n);
-        
+
         System.out.println("----Sorted Array----");
         for(int i=0;i<n;i++)
             System.out.print(a[i] + " ");

b2078d5cbd219d2eacb82ae5d1a30f203b883f48
4nh51rk <krishnarastogi21022006@gmail.com>
2026-08-09 13:51:51 +0000

    recursiveBubble

---
+++ Sorting_Algorithms/recursiveBubbleSorting.java
@@ -0,0 +1,35 @@
+package Java.Sorting_Algorithms;
+import java.util.*;
+public class recursiveBubbleSorting{
+    
+    static int[] recursiveBubble(int nums[], int n){
+        if(n == 1)
+            return nums;
+        
+        for(int i=0;i<n-1;i++){
+            if(nums[i] > nums[i+1]){
+                int temp = nums[i];
+                nums[i] = nums[i+1];
+                nums[i+1] = temp;
+            }
+        }
+        recursiveBubble(nums, n-1);
+        return nums;
+    }
+
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of the array: ");
+        int size = sc.nextInt();
+        int nums[] = new int[size];
+        System.out.print("Enter the elements in the array: ");
+        for(int i=0;i<size;i++)
+            nums[i] = sc.nextInt();
+
+        recursiveBubble(nums, size);
+        System.out.println("----Sorted Array----");
+        for(int i=0;i<size;i++)
+            System.out.print(nums[i] + " ");
+        System.out.println();
+    }
+}
